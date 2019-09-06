1e3ac07822e03140f3d93b090faacb988aaab522
MPink <menatpink@gmail.com>
2019-09-06 22:33:02 +0100

    Update to the three.js library

--- Universal Lights/app.js
+++ Universal Lights/app.js
@@ -11,30 +11,23 @@ const NEAR = 0.1;
 const FAR = 10000;
 
 // Get the DOM element to attach to
-const container = document.querySelector('body');
-
+const container = document.querySelector("body");
 
 // Create a WebGL renderer, camera and a scene
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setClearColor("#151718", 1);
-const camera = new THREE.PerspectiveCamera(
-    VIEW_ANGLE,
-    ASPECT,
-    NEAR,
-    FAR
-);
-
+const camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
 
 //create the sphere's material
 const sphereMaterial = new THREE.MeshLambertMaterial({
-    color: 0xCC00FF
+  color: 0xcc00ff
 });
 
 const scene = new THREE.Scene();
 
 //Add the camera to the scene
-camera.lookAt(new THREE.Vector3(0, 50, 0))
+camera.lookAt(new THREE.Vector3(0, 50, 0));
 scene.add(camera);
 
 // Start the renderer
@@ -51,7 +44,6 @@ const RINGS = 16;
 
 //Create a new mesh with sphere geometry - we will cover the sphereMaterial next!
 
-
 //     //Move the Sphere back in Z so we can see it
 // sphere.position.z = -300;
 
@@ -59,8 +51,8 @@ const RINGS = 16;
 scene.add(sphere);
 
 // create a point light
-const pointLight = new THREE.PointLight(0xFFFFFF);
-const pointLight2 = new THREE.PointLight(0xFFFFFF);
+const pointLight = new THREE.PointLight(0xffffff);
+const pointLight2 = new THREE.PointLight(0xffffff);
 
 // set its position
 pointLight.position.x = 0;
@@ -73,70 +65,71 @@ pointLight2.position.z = 1600;
 scene.add(pointLight);
 scene.add(pointLight2);
 
-
 function Star(rad, zpos, xpos, ypos) {
-    this.rad = rad;
-    this.zpos = zpos;
-    this.xpos = xpos;
-    this.ypos = ypos;
+  this.rad = rad;
+  this.zpos = zpos;
+  this.xpos = xpos;
+  this.ypos = ypos;
 }
-
 
 //This will add a starfield to the background of a scene
 var starsGeometry = new THREE.Geometry();
 
 for (var i = 0; i < 500; i++) {
+  var star = new THREE.Vector3();
+  star.x = THREE.Math.randFloatSpread(Math.round(WIDTH));
+  star.y = THREE.Math.randFloatSpread(Math.round(HEIGHT));
+  star.z = THREE.Math.randFloatSpread(3000);
 
-    var star = new THREE.Vector3();
-    star.x = THREE.Math.randFloatSpread(Math.round(WIDTH));
-    star.y = THREE.Math.randFloatSpread(Math.round(HEIGHT));
-    star.z = THREE.Math.randFloatSpread(3000);
-
-    starsGeometry.vertices.push(star);
-
+  starsGeometry.vertices.push(star);
 }
 
 function map(num, in_min, in_max, out_min, out_max) {
-    var numerator = (num - in_min) * (out_max - out_min);
-    var denominator = (in_max - in_min) + out_min;
-    return numerator / denominator;
+  var numerator = (num - in_min) * (out_max - out_min);
+  var denominator = in_max - in_min + out_min;
+  return numerator / denominator;
 }
-var randomTrueFalse = function () {
-    return Math.random() >= 0.5 ? true : false;
-}
+var randomTrueFalse = function() {
+  return Math.random() >= 0.5 ? true : false;
+};
 
 var circleArr = [];
 for (var i = 0; i < 500; i++) {
-    var colorVary;
-    var colorVary2;
-    var colorVary3;
+  var colorVary;
+  var colorVary2;
+  var colorVary3;
 
+  var material = new THREE.MeshLambertMaterial();
+  // { color: "rgb("+ colorVary2 +","+ colorVary3 +"," + colorVary + ")"}
 
-    var material = new THREE.MeshLambertMaterial();
-    // { color: "rgb("+ colorVary2 +","+ colorVary3 +"," + colorVary + ")"}
+  var sphere = new THREE.Mesh(
+    new THREE.CircleGeometry(RADIUS, SEGMENTS, RINGS),
+    material
+  );
 
-    var sphere = new THREE.Mesh(new THREE.CircleGeometry(RADIUS, SEGMENTS, RINGS), material);
+  sphere.position.x = THREE.Math.randFloatSpread(WIDTH);
+  sphere.position.y = THREE.Math.randFloatSpread(HEIGHT);
+  sphere.position.z = THREE.Math.randFloatSpread(3000);
+  sphere.rotationSpeed = i * 0.0000000007;
+  sphere.directionX = randomTrueFalse();
 
-    sphere.position.x = THREE.Math.randFloatSpread(WIDTH);
-    sphere.position.y = THREE.Math.randFloatSpread(HEIGHT);
-    sphere.position.z = THREE.Math.randFloatSpread(3000);
-    sphere.rotationSpeed = i * 0.0000000007;
-    sphere.directionX = randomTrueFalse();
+  colorVary =
+    map(sphere.position.x, -WIDTH / 2, WIDTH / 2, 0, 1) * Math.random();
+  colorVary2 =
+    map(sphere.position.y, -WIDTH / 2, WIDTH / 2, 0, 1) * Math.random();
+  colorVary3 =
+    map(sphere.position.z, -WIDTH / 2, WIDTH / 2, 0, 1) * Math.random();
 
-    colorVary = map(sphere.position.x, -WIDTH / 2, WIDTH / 2, 0, 1) * (Math.random());
-    colorVary2 = map(sphere.position.y, -WIDTH / 2, WIDTH / 2, 0, 1) * (Math.random());
-    colorVary3 = map(sphere.position.z, -WIDTH / 2, WIDTH / 2, 0, 1) * (Math.random());
-
-    sphere.material.color = {
-        r: colorVary2,
-        g: colorVary3,
-        b: colorVary,
-    }
-    circleArr.push(sphere);
-    scene.add(sphere);
+  sphere.material.color = {
+    r: colorVary2,
+    g: colorVary3,
+    b: colorVary
+  };
+  circleArr.push(sphere);
+  scene.add(sphere);
 }
 var starsMaterial = new THREE.PointsMaterial({
-    color: 0xffffff
+  color: 0xffffff
 });
 
 var starField = new THREE.Points(starsGeometry, starsMaterial);
@@ -144,57 +137,54 @@ var starField = new THREE.Points(starsGeometry, starsMaterial);
 scene.add(starField);
 
 // sphere geometry
-sphere.SphereGeometry
-
-
+sphere.SphereGeometry;
 
 function randomStraightLines(obj, speed) {
-    //For a certain amount of time,
-    clearInterval(function () {
-        // Select either the x, y or z property
-        var position;
-        var randomRoll = Math.random();
-        if (randomRoll <= 0.33) {
-            position = obj.rotation.x;
-        } else if (randomRoll <= 0.66) {
-            position = obj.rotation.y;
-        } else {
-            position = obj.rotation.z;
-        }
-    }, 3000);
-    //rotate i.property, multiplied by a speed property
-    obj.position += speed;
-    // Then select a new x, y or z property
-
+  //For a certain amount of time,
+  clearInterval(function() {
+    // Select either the x, y or z property
+    var position;
+    var randomRoll = Math.random();
+    if (randomRoll <= 0.33) {
+      position = obj.rotation.x;
+    } else if (randomRoll <= 0.66) {
+      position = obj.rotation.y;
+    } else {
+      position = obj.rotation.z;
+    }
+  }, 3000);
+  //rotate i.property, multiplied by a speed property
+  obj.position += speed;
+  // Then select a new x, y or z property
 }
 
 function rotatex(obj, speed) {
-    obj.rotation.x += speed;
+  obj.rotation.x += speed;
 }
 
 function rotateAccelerateX(obj, speed) {
-    obj.rotation.x += speed;
-    sphere.rotationSpeed += 0.0000001;
+  obj.rotation.x += speed;
+  sphere.rotationSpeed += 0.0000001;
 }
 
 function rotatey(obj, speed) {
-    obj.rotation.y += speed;
+  obj.rotation.y += speed;
 }
 
 function rotatez(obj, speed) {
-    obj.rotation.z += speed;
+  obj.rotation.z += speed;
 }
 
 function sceneRotate() {
-    scene.rotation.y += .0002;
+  scene.rotation.y += 0.0002;
 }
 
-window.addEventListener('resize', function () {
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    renderer.setSize(width, height);
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
+window.addEventListener("resize", function() {
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  renderer.setSize(width, height);
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
 });
 
 var controls = new THREE.OrbitControls(camera);
@@ -204,27 +194,24 @@ controls.update();
 var randomNumberX = 0.02;
 var randomNumberY = 0.02;
 
-(function () {
-    setInterval(
-        function () {
-            randomNumberX = Math.round(Math.random() * 10) / 100;
-            randomNumberY = Math.round(Math.random() * 10) / 100;
-        }, 1000)
+(function() {
+  setInterval(function() {
+    randomNumberX = Math.round(Math.random() * 10) / 100;
+    randomNumberY = Math.round(Math.random() * 10) / 100;
+  }, 1000);
 })();
 
-
-
 function update() {
-    // Draw
-    var timer = Date.now() * .00009;
-    // camera.rotation.y = Math.cos(timer);
-    renderer.render(scene, camera);
-    circleArr.forEach(function (i) {
-        rotatex(i, randomNumberX);
-        rotatey(i, randomNumberY);
-    });
-    controls.update();
-    requestAnimationFrame(update);
+  // Draw
+  var timer = Date.now() * 0.00009;
+  // camera.rotation.y = Math.cos(timer);
+  renderer.render(scene, camera);
+  circleArr.forEach(function(i) {
+    rotatex(i, randomNumberX);
+    rotatey(i, randomNumberY);
+  });
+  //   controls.update();
+  requestAnimationFrame(update);
 }
 
 // Schedule the first frame
